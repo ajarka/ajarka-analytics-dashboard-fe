1031951cd83e8920499ab04d30b7c0c89a6dc581
Aisyah Alhumairo <aisyahalhumairo08@gmail.com>
2025-05-20 13:04:09 +0700

    https://github.com/Smartelco/smartelco-analytics-dashboard-fe/issues/2

--- src/types/github.ts
+++ src/types/github.ts
@@ -51,7 +51,6 @@ export interface GithubIssue {
     start_date?: string;
     due_date?: string;
     html_url: string;
-    status: string;
     repository: {
         name: string;
         full_name: string;
